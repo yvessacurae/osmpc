d8a25154a77f0a812d7325391ae30d99b230d539
Yves Sacurae <yvessacurae@gmail.com>
2016-06-14 20:34:52 -0300

    Osm assist

--- changer.js
+++ changer.js
@@ -1,9 +1,8 @@
 var mentalityValue = "";
 var timeValue = "";
+var timeLabel = "";
+var mentalityLabel = "";
 var isMoving = false;
-
-var timeValue = document.getElementById('Tempo').getAttribute('value');
-
 
 window.addEventListener("load", function() {
 	mentalityValue = document.getElementById('Mentality').getAttribute('value');
@@ -16,6 +15,8 @@ window.addEventListener("load", function() {
 function init(){
 	if(mentalityValue!=='' && timeValue!==''){
 		changeInputs();
+		document.getElementById('spanMentalityVal').insertAfter('span');
+		document.getElementById('spanTempoVal').insertAfter('span');
 	}
 }
 
@@ -29,31 +30,36 @@ function changeInputs(){
 }
 
 var dragMentality = document.getElementById('divMentality');
+var dragTime = document.getElementById('divTempo');
 
 dragMentality.addEventListener("mousedown", function(){
+	
+	mentalityLabel = document.getElementById('spanMentalityVal').innerHTML.split(':')[0];
 
 	dragMentality.addEventListener("mousemove", function(){
 		mentalityValue = document.getElementById('Mentality').getAttribute('value');
+
+		document.getElementById('spanMentalityVal').innerText = mentalityLabel + ": " + mentalityValue + "%";
 	});
 
 });
 
-dragMentality.addEventListener("mouseup", function(){
-	var mentalityLabel = document.getElementById('spanMentalityVal').innerHTML;
-	document.getElementById('spanMentalityVal').innerText = mentalityLabel + ": " + mentalityValue + "%";
-});
-
-var dragTime = document.getElementById('divTempo');
-
 dragTime.addEventListener("mousedown", function(){
+
+	timeLabel = document.getElementById('spanTempoVal').innerHTML.split(':')[0];
 
 	dragTime.addEventListener("mousemove", function(){
 		timeValue = document.getElementById('Tempo').getAttribute('value');
+
+		document.getElementById('spanTempoVal').innerText = timeLabel + ": " + timeValue + "%";
 	});
 
 });
 
 dragTime.addEventListener("mouseup", function(){
-	var timeLabel = document.getElementById('spanTempoVal').innerHTML;
 	document.getElementById('spanTempoVal').innerText = timeLabel + ": " + timeValue + "%";
+});
+
+dragMentality.addEventListener("mouseup", function(){
+	document.getElementById('spanMentalityVal').innerText = mentalityLabel + ": " + mentalityValue + "%";
 });
